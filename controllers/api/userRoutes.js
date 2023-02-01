a00abedfb450a48e4170aa393a6ec0034c41082d
Audrey Alcantara <adrianaquin22@gmail.com>
2023-02-01 18:44:28 -0500

    more routes and auth

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router()
+const { Users } = require('../../models')
+const Users = require('../../models/Users')
+
+router.post('/', async(req, res) => {
+    try {
+        const newUser = await Users.create(req.body)
+        req.session.save(() => {
+            req.session.id = newUser.id
+            req.session.logged_in = true
+
+            res.status(200).json(newUser)
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+router.post('/login', async(req, res) => {
+    try {
+        const newUser = await Users.findOne({
+            where: {email: req.body.email}
+        })
+
+        if(!newUser){
+            res.status(404).json({message: 'incorrect email or password on file'})
+            return
+        }
+
+        const validPassword = await newUser.checkPassword(req.body.password)
+        if(!validPassword){
+            res.status(404).json({message: 'incorrect email or password on file'})
+            return
+        }
+
+        req.session.save(() => {
+            req.session.id = newUser.id
+            req.session.logged_in = true
+
+            res.json({ user: newUser, message: 'You are now logged in'})
+        })
+    } catch (err){
+        res.status(500).json(err)
+    }
+})
+
+router.post('/logout', (req, res) => {
+    if(req.session.logged_in){
+        req.session.destroy(() => {
+            res.status(204).end()
+        })
+    } else {
+        res.status(500).end()
+    }
+})
+
+module.exports = router
