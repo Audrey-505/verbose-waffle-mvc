e97589fda1943934476563cb863c3c79ac9e9ebc
Audrey Alcantara <adrianaquin22@gmail.com>
2023-02-03 14:21:41 -0500

    updated queryselectors in post js

--- public/js/post.js
+++ public/js/post.js
@@ -40,9 +40,9 @@ if (title && post) {
   };
 
   document
-  .querySelector('.new-project-form')
+  .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.post-list')
   .addEventListener('click', delButtonHandler);
