b6da02db5ca5a159cc618fde3bb6aac3368384ad
Audrey Alcantara <adrianaquin22@gmail.com>
2023-02-06 16:17:22 -0500

    added comment modules

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Post = require('./Post')
 const User = require('./User')
+const Comment = require('./Comment')
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
@@ -10,7 +11,25 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 })
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+
 module.exports = {
     Post,
-    User
+    User,
+    Comment
 }
