dae137360b06a3bcda8daf6d1db80f8c6722f2ce
Audrey Alcantara <adrianaquin22@gmail.com>
2023-02-18 16:06:58 -0500

    updated code for del com

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -28,16 +28,13 @@ router.put('/:id', withAuth, async(req, res) => {
 router.delete('/:id', withAuth, async(req, res) => {
     try {
         console.log('-----PARAMS',req.params.id)
-        const commentData = await Comment.destroy(req.body,{
-            user_id: req.session.user_test
-        },
-        {
+        const commentData = await Comment.destroy({
             where: {
-                // id: req.params.id,
-                id: req.body.id,
+                id: req.params.id,
+                //id: req.body.id,
                 
             }
-        })
+    })
         if(!commentData){
             res.status(404).json({ message: 'No comments with that ID were found' })
             return
